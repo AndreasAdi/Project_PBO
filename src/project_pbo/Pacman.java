fc4a86417fb337aca5349626e8466130fbd7c0c4
EnricoPucci01 <49854430+EnricoPucci01@users.noreply.github.com>
2019-11-18 16:40:57 +0700

    Tambahan property,contructor,getter,setter pacman

--- src/project_pbo/Pacman.java
+++ src/project_pbo/Pacman.java
@@ -10,5 +10,38 @@ package project_pbo;
  * @author Andreas
  */
 public class Pacman {
+    int x;
+    int y;
+    String image;
+
+    public Pacman(int x, int y, String image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
     
 }
